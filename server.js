b80e55fdad4aaf0b34ed6cf07c7c414079ab0fbc
Lucia <luciag.lara@gmail.com>
2021-06-11 01:00:09 +0200

    deploy5

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const morgan = require('morgan'); // ver por consola las peticiones que están l
 const path = require('path');
 const dbConfig = require('./Config/db.config');
 const port = process.env.PORT || 5000
-require('dotenv').config(); // rutas protegidas
+require('dotenv').config(); 
 
 
 // BBDD
 const db = require('./Models');
-const atlasUrl = process.env.DB_URI;
-const url = atlasUrl || `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`
+const atlasUrl = process.env.DB_MONGO_URL;
+const url = atlasUrl
 db.mongoose
   .connect(url, {
     useNewUrlParser: true,
@@ -48,11 +48,7 @@ app.use( cors(opcionesCors) );
 app.use(bodyParser.json()); // para poder recibir los datos en JSON, para que express pueda entenderlos
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '/build'))); // Servir los archivos estáticos de la aplicación React.En tripu path.resolve
 
-app.get('*', (req,res)=>{
-  res.sendFile(path.resolve(__dirname, '/build', 'index.html'))
-});
 //Rutas de autenticación y verificaciónClientgi
 require('./Routes/auth.routes')(app);
 require('./Routes/user.routes')(app);
@@ -61,6 +57,12 @@ require('./Routes/user.routes')(app);
 require('./Routes/ingredients.routes')(app)
 require('./Routes/recipes.routes')(app)
 
+app.use(express.static(path.join(__dirname, '/build'))); // Servir los archivos estáticos de la aplicación React.En tripu path.resolve
+
+app.get('*', (req,res)=>{
+  res.sendFile(path.resolve(__dirname, '/build', 'index.html'))
+});
+
 console.log(process.env)
 // simple route
 app.get("/", (req, res) => {
@@ -68,7 +70,7 @@ app.get("/", (req, res) => {
   });
 
 // Inicializamos el servidor
-app.listen(process.env.PORT || 27017, () =>{
+app.listen(port, '0.0.0.0', () =>{
     console.log(`El servidor esta funcionando en el puerto ${app.get('port')}`);
 
 })
